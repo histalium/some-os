ed6f2bbc42397312cb343b34d76f514511b30042
histalium <histalium@outlook.com>
2021-05-28 18:22:31 +0000

    print text on the screen

--- src/boot_sect_simple.asm
+++ src/boot_sect_simple.asm
@@ -1,3 +1,19 @@
+mov ah, 0x00
+mov al, 0x12
+int 0x10
+
+mov ah, 0x0e ; tty mode
+mov al, 'H'
+mov bl, 0x00
+
+print:
+    int 0x10     ; print H
+    cmp bl, 0x1f ; check if 31
+    je loop      ; if bl is 31 jump to loop
+
+    add bl, 0x01 ; next color
+    jmp print    ; print next
+
 ; A simple boot sector program that loops forever
 loop:
     jmp loop
